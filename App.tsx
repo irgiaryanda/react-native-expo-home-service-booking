28dee2cdcbd564c5c330cc3cf21434877f285c9e
Irgi <irgiarya1904@gmail.com>
2026-07-23 06:39:45 +0700

    chore: Initial Expo setup and enterprise folder structure

--- App.tsx
+++ App.tsx
@@ -5,6 +5,7 @@ import { Provider } from 'react-redux';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { store } from './src/store';
 import { AppNavigator } from './src/navigation/AppNavigator';
+import "./global.css";
 
 const queryClient = new QueryClient();
 
